862bf346eda04f33d0a9820622df9109586a5451
Kotetsu <bv19063@shibaura-it.ac.jp>
2021-09-26 00:30:07 +0900

    [add]画面表示の追加 GUI画面の表示部分の追加.

---
+++ src/main/java/GameDisplay.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class GameDisplay extends Display{
+    @Override
+    public void show(GraphicsInfo ginfo) {
+
+    }
+
+    @Override
+    public void loadMedia() throws IOException {
+
+    }
+}
